836bbdc5b10387bfe5f1c1083672c2ac2fc34af5
chef <cgcook81@gmail.com>
2019-01-29 23:23:08 -0500

    updated jquery

--- public/js/get.js
+++ public/js/get.js
@@ -1,12 +1,12 @@
 $(() => {
   // const currID = $(this).attr('#id');
 
-  $('.fav').on('click', event => {
+  $('li').on('click', event => {
     if(($(event.target).attr('id') === 'source')) {
-      $(event.target).parent().appendTo('#good');
+      $(event.target).attr('id','moved').appendTo('#good');
     } else {
-    $(event.target).appendTo('#bad');
+      $(event.target).appendTo('#bad');
   }
-  console.log($(event.target).attr('id'));
+  console.log($(event.target).parent());
 });
 });
