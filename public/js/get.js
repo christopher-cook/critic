a67cadf66a71c6995a4937f23293a471cadd92a4
chef <cgcook81@gmail.com>
2019-01-30 02:33:02 -0500

    added search functionality to page

--- public/js/get.js
+++ public/js/get.js
@@ -1,12 +1,32 @@
 $(() => {
   // const currID = $(this).attr('#id');
 
-  $('li').on('click', event => {
-    if(($(event.target).attr('id') === 'source')) {
-      $(event.target).attr('id','moved').appendTo('#good');
-    } else {
-      $(event.target).appendTo('#bad');
-  }
-  console.log($(event.target).parent());
+
+
+  $('#mySearch').on('click', event => {
+    const moviesReturned = [];
+    const searchValue = $('#search').val();
+    $.get(
+        {
+          url: 'https://www.omdbapi.com/?apikey=718f1911&s='+searchValue,
+          success: (data) => {
+            moviesReturned.push(data);
+
+            // let movieTitle = moviesReturned["Search"][i].Title;
+            // let movieYear = moviesReturned["Search"][i].Year;
+            // let moviePlot = moviesReturned["Search"][i].Plot;
+
+            for (let i = 0; i < 5; i++) {
+              const $li = $('<li>').text(moviesReturned[0]["Search"][i].Title).appendTo('.moviesDisplay');
+                          // $('<li>').text(movieYear[i]).appendTo('.moviesDisplay');
+                          // $('<li>').text(moviePlot[i]).appendTo('.moviesDisplay');
+          }
+        }
+    });
+    console.log(moviesReturned);
 });
+
 });
+
+
+// db["search"][0].Title
