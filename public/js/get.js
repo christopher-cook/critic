a6c0ac58fc7cc845c338d96913638fca644c2762
chef <cgcook81@gmail.com>
2019-01-30 18:28:06 -0500

    updated colujmn layouts for movies

--- public/js/get.js
+++ public/js/get.js
@@ -1,6 +1,4 @@
 $(() => {
-  // const currID = $(this).attr('#id');
-
 
 
   $('#mySearch').on('click', event => {
@@ -13,16 +11,33 @@ $(() => {
             moviesReturned.push(data);
 
             for (let i = 0; i < 5; i++) {
-              const $li = $('<li>').text(moviesReturned[0]["Search"][i].Title).appendTo('.moviesDisplay');
-                          $('<li>').text(moviesReturned[0]["Search"][i].Year).appendTo('.moviesDisplay');
-                          $('<li>').prepend("<img src ="+ moviesReturned[0]["Search"][i].Poster +">").appendTo('.moviesDisplay');
+              const $li = $('<li>').text(moviesReturned[0]["Search"][i].Title).appendTo('.moviesDisplay').appendTo('.column1');
+                          $('<li>').text(moviesReturned[0]["Search"][i].Year).appendTo('.moviesDisplay').appendTo('.column1');
+                          $('<li>').prepend("<img src ="+ moviesReturned[0]["Search"][i].Poster +"><br /><br />").appendTo('.moviesDisplay').appendTo('.column1');
+                          $('<button>').attr('id', 'selected').attr('omdbID', moviesReturned[0]["Search"][i].imdbID).text('select').appendTo('.moviesDisplay').appendTo('.column1');
+
           }
         }
     });
-    console.log(moviesReturned);
-});
-
 });
 
 
-// db["search"][0].Title
+  // $('#selected').on('click', event => {
+  //   // const movieSelected = [];
+  //   // const selectedValue = $('omdbID').val();
+  //   console.log('hello');
+  // //     $.get(
+  // //       {
+  // //         url: 'https://www.omdbapi.com/?apikey=718f1911&'+selectedValue,
+  // //         success: (data) => {
+  // //           // movieSelected.push(data);
+  // //
+  // //           // for (let i = 0; i < 1; i++) {
+  // //           //   const $li = $('<li>').text(movieSelected[0].Title).appendTo('.moviesDisplay');
+  // //           //               $('<li>').text(movieSelected[0].Plot).appendTo('.moviesDisplay');
+  // //           //
+  // //           // }
+  // //
+  // // });
+// });
+});
