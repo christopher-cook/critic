7ae1e97c5eac6791414f3488729f1d7fb8df306b
chef <cgcook81@gmail.com>
2019-01-30 23:07:48 -0500

    updated jquery

--- public/js/get.js
+++ public/js/get.js
@@ -2,42 +2,25 @@ $(() => {
 
 
   $('#mySearch').on('click', event => {
-    const moviesReturned = [];
     const searchValue = $('#search').val();
     $.get(
         {
           url: 'https://www.omdbapi.com/?apikey=718f1911&s='+searchValue,
           success: (data) => {
-            moviesReturned.push(data);
-
             for (let i = 0; i < 5; i++) {
-              const $li = $('<li>').text(moviesReturned[0]["Search"][i].Title).appendTo('.moviesDisplay').appendTo('.column1');
-                          $('<li>').text(moviesReturned[0]["Search"][i].Year).appendTo('.moviesDisplay').appendTo('.column1');
-                          $('<li>').prepend("<img src ="+ moviesReturned[0]["Search"][i].Poster +"><br /><br />").appendTo('.moviesDisplay').appendTo('.column1');
-                          $('<button>').attr('id', 'selected').attr('omdbID', moviesReturned[0]["Search"][i].imdbID).text('select').appendTo('.moviesDisplay').appendTo('.column1');
+              if(data.Response === "False") {
+                alert('Movie not found, please try again!');
+                return;
+              } else {
+              const $li = $('<li>').text(data["Search"][i].Title).attr('id', 'pending').appendTo('.moviesDisplay').appendTo('.column1');
+                          $('<li>').text(data["Search"][i].Year).appendTo('.moviesDisplay').attr('id', 'pending').appendTo('.column1');
+                          $('<li>').prepend("<img src ="+ data["Search"][i].Poster +"><br /><br />").attr('id', 'pending').appendTo('.moviesDisplay').appendTo('.column1');
+                          // const $button = $('<button>').attr('id', '').text('select').appendTo('.moviesDisplay').appendTo('.column1');
+                          // $button.on('click',
 
+              }
           }
         }
     });
 });
-
-
-  // $('#selected').on('click', event => {
-  //   // const movieSelected = [];
-  //   // const selectedValue = $('omdbID').val();
-  //   console.log('hello');
-  // //     $.get(
-  // //       {
-  // //         url: 'https://www.omdbapi.com/?apikey=718f1911&'+selectedValue,
-  // //         success: (data) => {
-  // //           // movieSelected.push(data);
-  // //
-  // //           // for (let i = 0; i < 1; i++) {
-  // //           //   const $li = $('<li>').text(movieSelected[0].Title).appendTo('.moviesDisplay');
-  // //           //               $('<li>').text(movieSelected[0].Plot).appendTo('.moviesDisplay');
-  // //           //
-  // //           // }
-  // //
-  // // });
-// });
 });
